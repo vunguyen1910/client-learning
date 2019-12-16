bae0fac982f614034b7bf310395c706d6db3206c
Viktor_nguyen <melodycenter19@gmail.com>
2019-12-17 01:54:25 +0700

    done

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -4,20 +4,21 @@ import { Row, Col, Image } from "react-bootstrap";
 export default function AboutUs() {
   return (
     <Row className="Group_109_Class mt-5">
-      <Col className ="text-right justify-content-center pb-5">
-        <img src={require("../img/Music-on-brain.jpg")}/>
+      <Col className="text-right justify-content-center pb-5">
+        <img src={require("../img/Music-on-brain.jpg")} />
       </Col>
-      <Col style={{padding: "8%"}}>
+      <Col style={{ padding: "8%" }}>
         <div class="Group_3_Class">
           <div class="Science_Class">
             <span>Science</span>
           </div>
           <div data-type="Text" class="Lorem_ipsum_dolor_sit_amet__co_Class">
             <span>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliqui
+              Music associated with our life. It's hard to imagine a world that
+              lacks the notes: from mother lull you to sleep, instrumental for
+              coffee Saturday afternoon, song honoring her pupil for graduation,
+              to the tune thrill for wedding couple. Even when he is sad,
+              painful, we also seek to melodies soothe the spirit.
             </span>
           </div>
         </div>
@@ -27,7 +28,7 @@ export default function AboutUs() {
             data-name="Learn anything"
             class="Learn_anything_Class"
           >
-            <img src={require('../img/icon_1.png')} className="pr-3"/>
+            <img src={require("../img/icon_1.png")} className="pr-3" />
             <span>Learn anything</span>
           </div>
           <div
@@ -35,7 +36,7 @@ export default function AboutUs() {
             data-name="Talk to our instructors"
             class="Talk_to_our_instructors_Class pt-2"
           >
-            <img src={require('../img/icon_2.png')} className="pr-3"/>
+            <img src={require("../img/icon_2.png")} className="pr-3" />
             <span>Talk to our instructors</span>
           </div>
           <div
@@ -43,7 +44,7 @@ export default function AboutUs() {
             data-name="Speak with others"
             class="Speak_with_others_Class pt-2"
           >
-            <img src={require('../img/icon_3.png')} className="pr-3"/>
+            <img src={require("../img/icon_3.png")} className="pr-3" />
             <span>Speak with others</span>
           </div>
         </div>
