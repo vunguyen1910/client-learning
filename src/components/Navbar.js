95fe8d3176ac31c51584c97606ac5d2ecca5d31c
Viktor_nguyen <melodycenter19@gmail.com>
2019-12-23 14:38:27 +0700

    fix navbar redirect

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -22,7 +22,11 @@ export default function NavBar(props) {
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark">
-        <img className="navbar-brand ml-5" href="#" src={require('../img/group-2.png')}/>
+        <img
+          className="navbar-brand ml-5"
+          href="#"
+          src={require("../img/group-2.png")}
+        />
         <button
           className="navbar-toggler"
           type="button"
@@ -36,27 +40,36 @@ export default function NavBar(props) {
         </button>
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
-            
-          </ul>
+          <ul className="navbar-nav mr-auto"></ul>
           <ul className="navbar-nav col-auto">
-          <li className="nav-item">
-              <LinkTo className="nav-link border-right active px-5" >
+            <li className="nav-item">
+              <LinkTo className="nav-link border-right active px-5">
                 Courses
               </LinkTo>
             </li>
-            <li className="nav-item">
-              <Link className="nav-link active mx-5">
-                Login
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-                className="nav-link btn btn-outline-secondary rounded-pill active px-3"
-              >
-                Become A Tutor
-              </Link>
-            </li>
+            {props.currentUser ? (
+              <>
+                <li className="nav-item">
+                  <p className="nav-link active mx-5" onClick={()=>logOut()}>Logout</p>
+                </li>
+              </>
+            ) : (
+              <>
+                <li className="nav-item">
+                  <Link className="nav-link active mx-5" to="/login">
+                    Login
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link
+                    className="nav-link btn btn-outline-secondary rounded-pill active px-3"
+                    to="/register"
+                  >
+                    Become A Tutor
+                  </Link>
+                </li>
+              </>
+            )}
           </ul>
         </div>
       </nav>
